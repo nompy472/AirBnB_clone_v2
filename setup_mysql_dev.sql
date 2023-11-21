d1e5108d5db06c0aaee44c14fa290e0c01a60717
Siphiwe <Nompychiloane@gmail.com>
2023-11-21 11:03:30 -0800

    Setup test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
--- Create or use an existing database
+-- Creates or use an existing database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create or use an existing user
+-- Creates or use an existing user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the hbnb_test_db to hbnb_test
+-- Grants all privileges on the hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Grant SELECT privilege on performance_schema to hbnb_test
+-- Grants SELECT privilege on performance_schema to hbnb_test
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
--- Flush privileges to apply the changes
+-- Flushes privileges to apply the changes
 FLUSH PRIVILEGES;
