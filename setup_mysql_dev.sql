4ca1347da3f2f047742f948be2db56bbde3329be
zagaouch <i.zagaouch@gmail.com>
2023-11-21 23:23:36 +0100

    fix task Console improvements

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,5 @@
--- Creates or use an existing database
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Creates or use an existing user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grants all privileges on the hbnb_test_db to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grants SELECT privilege on performance_schema to hbnb_test
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flushes privileges to apply the changes
-FLUSH PRIVILEGES;
+-- sql requiest that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
