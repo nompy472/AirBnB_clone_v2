ce31c94eafd44e2b70caa9e7862d42cbeae6c975
zagaouch <i.zagaouch@gmail.com>
2023-11-21 22:37:57 +0100

    fix task Console improvements

--- console.py
+++ console.py
@@ -112,7 +112,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
-
+    
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
