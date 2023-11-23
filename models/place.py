bbc7caf3a3de4cb3ad6f6caa9d2d62ea7d0c50b2
zagaouch <i.zagaouch@gmail.com>
2023-11-23 01:20:22 +0100

    fix task 6

--- models/place.py
+++ models/place.py
@@ -38,7 +38,8 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", backref="place",
                                cascade="all, delete, delete-orphan")
         amenities = relationship(
-            "Amenity", secondary="place_amenity", backref="places", viewonly=False)
+            "Amenity", secondary="place_amenity",
+            backref="places", viewonly=False)
     else:
         city_id = ""
         user_id = ""
